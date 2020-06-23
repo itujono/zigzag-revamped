d82d1376b7514b79ace3855d3a18101e3955efe6
riva <rivayudha@gmail.com>
2020-06-23 09:34:20 +0700

    Order item @ History order.

--- src/pages/Profile/HistoryOrder/OrderItems.js
+++ src/pages/Profile/HistoryOrder/OrderItems.js
@@ -1,17 +1,36 @@
+/* eslint-disable react/display-name */
 import React from "react"
-import { Table } from "antd"
+import { Table, Avatar } from "antd"
 import { pricer, mobile } from "helpers"
 
 export default function OrderItems({ data }) {
 	const columns = [
-		{ title: "Nama", dataIndex: "product_name", key: "product_name" },
-		{ title: "Harga", dataIndex: "product_price", key: "product_price", render: data => `Rp ${pricer(data)}` },
-		{ title: "Kuantitas", dataIndex: "product_qty", key: "product_qty" },
+		{
+			title: "Nama",
+			key: "product_name",
+			render: (data) => (
+				<div>
+					<Avatar src={data.product_order_detail?.product_image.picture} />
+					&nbsp; {data.product_name}
+				</div>
+			)
+		},
+		{
+			title: "Kategori",
+			dataIndex: "product_order_detail",
+			key: "category",
+			render: ({ categories }) => categories.name
+		},
+		{
+			title: "Harga",
+			key: "product_price",
+			render: (data) => data.product_qty + " x " + data.product_price
+		},
 		{
 			title: "Total",
 			dataIndex: "product_total_price",
 			key: "product_total_price",
-			render: data => `Rp ${pricer(data)}`
+			render: (data) => `Rp ${pricer(data)}`
 		}
 	]
 
