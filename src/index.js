78ff9060a731c5fae6fc2bd937ba9e13b40ea7dc
riva <rivayudha@gmail.com>
2020-08-17 18:08:45 +0700

    0.2.22

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import moment from "moment"
-import "react-image-lightbox/style.css"
 import "antd/dist/antd.css"
 import "./styles/index.less"
 import App from "./App"
