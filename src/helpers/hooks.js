f892660fa964289ef4c48eeece92fe695a088835
riva <rivayudha@gmail.com>
2021-01-14 12:08:59 +0700

    0.3.2

--- src/helpers/hooks.js
+++ src/helpers/hooks.js
@@ -67,17 +67,11 @@ export const useUserIp = () => {
 export function useDebounce(value, delay = 800) {
 	const [debouncedValue, setDebouncedValue] = useState(value)
 
-	useEffect(
-		() => {
-			const handler = setTimeout(() => {
-				setDebouncedValue(value)
-			}, delay)
-			return () => {
-				clearTimeout(handler)
-			}
-		},
-		[value, delay] // Only re-call effect if value or delay changes
-	)
+	useEffect(() => {
+		const handler = setTimeout(() => setDebouncedValue(value), delay)
+
+		return () => clearTimeout(handler)
+	}, [value, delay])
 
 	return debouncedValue
 }
